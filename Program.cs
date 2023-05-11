aefca883c11357edc9111540ab2d9c9485a20890
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2023-05-11 15:46:06 +1200

    fixed

--- Program.cs
+++ Program.cs
@@ -5,12 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
-            Console.WriteLine("Hopefully this breaks things")
+
+            Console.WriteLine("Hopefully this breaks things");
             Console.ReadLine();
-=======
+
             Console.WriteLine("Welp conflict");
->>>>>>> c75181ab1ce96dc91fe0dad1ede7c207cd02b1d0
+
+            while (true) 
+            {
+                Console.WriteLine(" EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+            } 
+
+
         }
     }
 }
